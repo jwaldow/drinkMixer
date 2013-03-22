6d24dbadd914c4c507865e18a50f6d27c2d95c65
Jon Waldow <jwaldow@mines.edu>
2013-03-21 23:08:18 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrinkMixer::Application.config.secret_token = 'bb647522f4c9674df69753b20fd3816d5d578ef8013a031d925581bd0dc7b01a81dcff94f85c5692d238e1eda4b864375cb80f0518517a8b14ac0039bf46dcef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DrinkMixer::Application.config.session_store :cookie_store, key: '_drinkMixer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DrinkMixer::Application.config.session_store :active_record_store
