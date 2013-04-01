cb1d84f4a868e3dd1cdd5d6639925c018688e60d
Jon Waldow <jwaldow@mines.edu>
2013-03-31 20:32:35 -0600

    can now create users and generated sessions as well as admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 DrinkMixer::Application.routes.draw do
+  get "admin/index"
+
+  get "sessions/new"
+
+  get "sessions/create"
+
+  get "sessions/destroy"
+
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
