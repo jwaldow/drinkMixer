136421701ad668211b4939217b337240f34c7192
Jon Waldow <jwaldow@mines.edu>
2013-04-12 19:19:04 -0600

    added creation of cart private methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
   #before_filter :authorize
   protect_from_forgery
 
+  private
+
+  	def current_cabinet
+  		Cabinet.find(session[:cart_id])
+  	rescue ActiveRecord::RecordNotFound
+  		cabinet = Cabinet.create
+  		session[:cabinet_id] = cabinet.id 
+  		cabinet
+  	end
+
   protected
 	def authorize
 		unless User.find_by_id(session[:user_id])
